7f6826cd71d4402b3d2b8dc679a3bc7e3fa34cad
Dhiraj1011 <159097901+Dhiraj1011@users.noreply.github.com>
2025-07-28 21:19:41 +0530

    Add files via upload

---
+++ Luhn algorithm (mod 10)  Credit card number.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+number=[]
+no=[0]*16
+num=[]
+add=0
+dig=[]
+print(number,no,num)
+
+
+# In[3]:
+
+
+
+print("enter the card number")
+number=input()
+
+
+for i in range(0,len(number)):
+    n=int(number[i])
+    num.append(n)
+    print(number[i],num[i])
+
+    
+if len(number)==16:
+    print("Thank you")
+else:
+    print("Number of digits entered are wrong")
+
+print(len(num),len(no))
+
+
+# In[4]:
+
+
+
+for i in range(0,len(number)):
+    if i%2==0:
+        no[i]=num[i]*2
+    else:
+        no[i]=num[i]
+        
+
+print("no=",no)
+print(len(no))
+
+
+# In[5]:
+
+
+for i in range(0,len(no)):
+    d=int(no[i]/10)
+    e=no[i]%10
+    print(d,e)
+    if d!=0:
+        dig.append(d)
+    if e!=0:
+        dig.append(e)
+        
+print(dig)
+
+
+# In[6]:
+
+
+for i in range(0,len(dig)):
+    add=add+dig[i]
+print(add)
+
+
+# In[13]:
+
+
+print(number)
+if add%10==0:
+    print("Entered credit/debit card number exists.")
+    if num[0]==3:
+        print("The card is  American Express, JCB")
+    elif num[0]==4:
+        print("The card is  Visa")
+    elif num[0]==5:
+        print("The card is  Mastercard")
+    elif num[0]==6:
+        print("The card is  Discover, RuPay")
+    else:
+        print("Can't dertermine")
+else:
+    print("Entered credit/debit card number does not exist.")
+    
+    
+
+
+# In[ ]:
+
+
+
+
